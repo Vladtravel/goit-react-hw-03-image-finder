aaab527fbd5597b2b6e1bcf7d551e3b99234231f
Vladyslav <vladtravel2015@gmail.com>
2021-07-21 19:04:21 +0300

    finish home work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Searchbar from "./components/Searchbar/Searchbar";
+import React, { Component } from "react";
+import ImageGallery from "./components/ImageGallery";
+import ServiseApi from "./service/ImageApi";
+import Button from "./components/Button";
+import Modal from "./components/Modal";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    query: "",
+    page: 1,
+    perPage: 12,
+    pictures: [],
+    currentPictures: "",
+    loader: false,
+    openModal: false,
+  };
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page, pictures } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.fetchPictures().then(() => {
+        this.loaderToggle();
+        if (pictures.length > 10) {
+          window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: "smooth",
+          });
+        }
+      });
+    }
+  }
+
+  OnLoadMore = () => {
+    this.setState((prev) => ({
+      page: prev.page + 1,
+    }));
+  };
+
+  onImgClick = (e) => {
+    if (e.target.nodeName !== "IMG") {
+      return;
+    }
+    this.setState({
+      currentPictures: e.target.dataset.img,
+    });
+    this.toggleModal();
+  };
+
+  loaderToggle = () => {
+    this.setState((prev) => ({
+      loader: !prev.loader,
+    }));
+  };
+
+  onImgOverviewClose = (e) => {
+    if (e.target.className.contains("Overlay")) {
+      this.toggleModal();
+    }
+  };
+
+  fetchPictures = () => {
+    const { query, page, perPage } = this.state;
+
+    this.loaderToggle();
+    const settings = {
+      query: query,
+      page: page,
+      perPage: perPage,
+    };
+    return ServiseApi.getImages(settings).then((hits) => {
+      this.setState((prev) => ({
+        pictures: [...prev.pictures, ...hits],
+      }));
+    });
+  };
+
+  onSubmit = (searchQuery) => {
+    this.setState({
+      query: searchQuery,
+      page: 1,
+      pictures: [],
+    });
+  };
+  toggleModal = () => {
+    this.setState((prev) => ({
+      openModal: !prev.openModal,
+    }));
+  };
+  render() {
+    const { pictures, query, loader, openModal, currentPictures } = this.state;
+    return (
+      <div className="App">
+        {loader && (
+          <Modal>
+            <Loader type="Rings" color="#00BFFF" height={700} width={700} />
+          </Modal>
+        )}
+        <Searchbar value={query} onFormSubmit={this.onSubmit} />
+        <ImageGallery pictures={pictures} onImgClick={this.onImgClick} />
+        {pictures.length > 0 && (
+          <Button
+            onBtnClick={this.OnLoadMore}
+            text={loader ? "Загружаем" : "Загрузить еще"}
+          />
+        )}
+
+        {openModal && (
+          <Modal onCloseModal={this.toggleModal}>
+            <img src={currentPictures} alt="Dont Worry Be Happy" />
+          </Modal>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
